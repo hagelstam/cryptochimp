71d18b5166caeeb305fefd2d4d341f3ac3640908
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-01-10 14:14:49 +0200

    feat: improve page load speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@/components/ThemeProvider';
+import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Suspense } from 'react';
